47524fc6d21ca9f0b9befdcf9eed686f1a86ff4e
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-11 20:29:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ from sklearn.metrics import (
 
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 
-train, test = train_test_split(df, test_size=0.2)
+# Assuming 'stroke' is the column you want to predict
+x = df.iloc[:, :-1]  # Features
+y = df.iloc[:, -1] 
 
-# Build a Gaussian Classifier
+# Split the dataset into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Build a Gaussian Naive Bayes Classifier
 model = GaussianNB()
 
 # Model training
-model.fit(train)
+model.fit(x_train, y_train)
 
 # Predict Output
-predicted = model.predict(test)
+y_pred = model.predict(x_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
