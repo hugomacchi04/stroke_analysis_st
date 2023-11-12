c9f7b044be37819988cfb52f33ec97df5feb55b0
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-11 20:05:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
+from sklearn.metrics import r2_score
 
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 
 train = df[:80]
 test = df[80:]
 
-st.table(test)
+mymodel = numpy.poly1d(numpy.polyfit(train, train, 4))
+
+r2 = r2_score(test, mymodel(test))
+
+st.write(r2)
