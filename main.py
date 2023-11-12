b9790a79ab8f6986334656f956ca4f2c404de9cc
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-11 21:01:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ from sklearn.metrics import (
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 
 df['gender_code'], genders = pd.factorize(df['gender'])
+df['married_code'], married = pd.factorize(df['ever_married'])
+df['work_code'], works = pd.factorize(df['work_type'])
+df['residence_code'], residences = pd.factorize(df['Residence_type'])
+df['smoking_code'], smokes = pd.factorize(df['smoking_status'])
 
-st.write(df)
+columns_to_drop = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
+df = df.drop(columns=columns_to_drop)
+
+# Assuming 'stroke' is the column you want to predict
+x = df.drop('stroke', axis=1)  # Features
+y = df['stroke']  # Target variable
+
+st.write(x)
+st.write(y)
 
 
