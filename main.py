fc4813f36d55b15b8b3274873d1f744c2946a0ce
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-11 21:03:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,21 @@ df = df.drop(columns=columns_to_drop)
 x = df.drop('stroke', axis=1)  # Features
 y = df['stroke']  # Target variable
 
-st.write(x)
-st.write(y)
+# Split the dataset into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Build a Gaussian Naive Bayes Classifier
+model = GaussianNB()
+
+# Model training
+model.fit(x_train, y_train)
+
+# Predict Output
+y_pred = model.predict(x_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+
+st.write(accuracy)
 
 
