bb6bfaa37c75c557945e85ab3083a3148b6a634d
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-11 20:22:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import (
+  GaussianNB,
+  accuracy_score,
+  confusion_matrix,
+  ConfusionMatrixDisplay,
+  f1_score,
+)
 
 df = pd.read_csv('healthcare-dataset-stroke-data.csv')
 
 train, test = train_test_split(df, test_size=0.2)
 
-st.write(test)
+# Build a Gaussian Classifier
+model = GaussianNB()
+
+# Model training
+model.fit(train, train)
+
+# Predict Output
+predicted = model.predict(test)
+accuracy = accuracy_score(predicted, test)
+f1 = f1_score(predicted, test, average="weighted")
+
+st.write(accuracy)
