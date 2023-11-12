f7ba1ac63448dbd4391fc22fa34a93267e8ac5b2
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-12 11:21:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,20 +23,29 @@ df = df.drop(columns=columns_to_drop)
 
 df = df.dropna()
 
-st.write(genders)
+x = df.drop('stroke', axis=1)  # features
+y = df['stroke']  # stroke
 
-# Assuming 'stroke' is the column you want to predict
-x = df.drop('stroke', axis=1)  # Features
-y = df['stroke']  # Target variable
-
-# Split the dataset into training and testing sets
+# split the dataset into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# Build a Gaussian Naive Bayes Classifier
+# build a Gaussian Naive Bayes Classifier
 model = GaussianNB()
 
-# Model training
+# model training
 model.fit(x_train, y_train)
+
+# create x_test
+with st.form("stroke_form"):
+    st.selectbox(
+        'What is your gender?',
+        genders
+    )
+
+    
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.success("Response successfully submitted")
 
 
 
