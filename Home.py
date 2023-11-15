08dce86596c3e49919ee35da6f47baf2c94e2fbe
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 16:15:03 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 '## What is a stroke?'
 st.write('Stroke accounts for 5% of annual deaths in the United States, equating to 1 in 20 fatalities.')
-st.write('A stroke may happen when there is a blockage in the blood flow to the brain or if there is sudden bleeding within the brain. There are five types of strokes: ischemic strokes, hemorrhagic strokes, transient ischemic attacks, cryptogenic strokes and brain stem strokes.')
+st.write('A stroke may happen when there is a blockage in the blood flow to the brain or if there is sudden bleeding within the brain. There are two types of strokes: ischemic strokes and hemorrhagic strokes.')
+st.write('Ischemic strokes are the most common kind of strokes. These occur when blood flow to the brain is blocked. Hemorrhagic strokes occur when the brain bleeds; this usually involves a blood vessel bursting, creating pressure in the nearby brain tissue')
 
 '## What are symptoms of a stroke?'
 st.write('•  Sudden numbness or weakness in the face, arm, or leg, especially on one side of the body.')
@@ -19,4 +20,5 @@ st.write('•  Sudden severe headache with no known cause.')
 
 '##### References'
 st.write('https://www.nhlbi.nih.gov/health/stroke')
+st.write('https://www.cdc.gov/stroke/signs_symptoms.htm')
 
