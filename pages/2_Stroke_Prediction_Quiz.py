01106993e62e9441f6b16c357243e796aa1b0526
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 17:33:48 -0500

    Update 2_Stroke_Prediction_Quiz.py

--- pages/2_Stroke_Prediction_Quiz.py
+++ pages/2_Stroke_Prediction_Quiz.py
@@ -42,12 +42,7 @@ model = LogisticRegression(random_state=42)
 # train the model
 model.fit(x_train, y_train)
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-  col4, col5, col6 = st.columns(3)
-  with col4:
-    st.image('4da0cbeab3bad859bc79b726c136725f-ribbon-stroke-health.png')
+st.image('line.png')
 
 # create test
 with st.form("stroke_form"):
