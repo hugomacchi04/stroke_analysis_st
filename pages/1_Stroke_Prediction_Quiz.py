770f60bdbf8715a670ab156b817a03bd52d7fa96
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 15:48:50 -0500

    Update 1_Stroke_Prediction_Quiz.py

--- pages/1_Stroke_Prediction_Quiz.py
+++ pages/1_Stroke_Prediction_Quiz.py
@@ -44,8 +44,10 @@ model.fit(x_train, y_train)
 
 col1, col2, col3 = st.columns(3)
 
-with col1:
-  st.image('4da0cbeab3bad859bc79b726c136725f-ribbon-stroke-health.png')
+with col2:
+  col4, col5, col6 = st.columns(3)
+  with col5:
+    st.image('4da0cbeab3bad859bc79b726c136725f-ribbon-stroke-health.png')
 
 # write accuracy
 y_pred = model.predict(x_test)
