1faf2681e6c950e948cee2c1560be25984960b8a
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 16:23:01 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 '## What is a stroke?'
 st.write('Stroke accounts for 5% of annual deaths in the United States, equating to 1 in 20 fatalities.')
 st.write('A stroke may happen when there is a blockage in the blood flow to the brain or if there is sudden bleeding within the brain. There are two types of strokes: ischemic strokes and hemorrhagic strokes.')
-st.write('Ischemic strokes are the most common kind of strokes. These occur when blood flow to the brain is blocked. Hemorrhagic strokes occur when the brain bleeds; this usually involves a blood vessel bursting, creating pressure in the nearby brain tissue')
+st.write('Ischemic strokes are the most common kind of strokes. These occur when blood flow to the brain is blocked. Hemorrhagic strokes occur when the brain bleeds; this usually involves a blood vessel bursting, creating pressure in the nearby brain tissue.')
 
 '## What are symptoms of a stroke?'
 st.write('•  Sudden numbness or weakness in the face, arm, or leg, especially on one side of the body.')
@@ -13,6 +13,8 @@ st.write('•  Sudden trouble walking, dizziness, loss of balance, or lack of co
 st.write('•  Sudden severe headache with no known cause.')
 
 '## Who has a higher probability of having a stroke?'
+st.write('There are a slew of factors that contribute to a higher likelihood of having a stroke. Some of these factors can be changes whereas others cannot.')
+st.write('Some factors include gender, race, age, high blood pressure, heart disease, smoking, obesity and so on. Men are more likely to have a stroke, especially the older they get. The is an even higher risk in African Americans due to high blood pressure.')
 
 '## What are prevention methods?'
 
