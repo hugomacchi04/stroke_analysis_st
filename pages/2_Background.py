84eae002845ac19a245b36f74dbd9c164f75be4b
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 17:10:59 -0500

    Create 2_Background.py

---
+++ pages/2_Background.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+'## Personal History'
+st.write('In late May 2023, my grandfather suffered a hemorrhagic stroke, displaying classic symptoms that, unfortunately, went unrecognized by my family due to our lack of awareness. A month prior, he began experiencing double vision in both eyes, a sign we now know we should have taken more seriously. About a month before he had the stroke, my grandpa started having double vision in both eyes. The doctors noticed blood, but assumed that it would eventually reabsorb back into the brain.')
+st.write('Towards the end of May, he developed a severe migraine out of nowhere. My grandparents decided to give it few days to resolve itself, but the turning point was when my grandpa forgot how to dress himself for work and dropped his sandwich unknowingly while walking into the office. After arriving to the ER, he was eventually given surgery to remove some of the blood on one side of his head. For two weeks in the ICU, he seemed in a vegetative state. Though he briefly woke up, doctors were not optimistic, prompting funeral arrangements and a decision to transfer him to hospice.')
+st.write('This is where the story takes a turn. While in hospice, he woke up, started talking, swallowed food for the first time and moved his supposedly paralyzed arm and leg. We could not believe it! After a week in hospice, my grandpa started therapy and was back home within a month.')
+st.write('Nowdays he is able to walk on his own and his personality remains the same. However, any minor infection triggers symptoms. While grateful for the positive outcome, I cannot help but wonder if early recognition could have prevented this.')
+st.write('Witnessing the impact on both my grandpa and grandma, I do not want anyone to go through something like this. Therefore, I hope to create awareness of the symptoms and signs of a stroke with this app!')
+  
