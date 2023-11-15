222624fa4d2f0d5bfcca2e0c52b02a44dc117df2
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 17:11:55 -0500

    Update 1_Background.py

--- pages/1_Background.py
+++ pages/1_Background.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+ol1, col2, col3 = st.columns(3)
+
+with col2:
+  col4, col5, col6 = st.columns(3)
+  with col5:
+    st.image('4da0cbeab3bad859bc79b726c136725f-ribbon-stroke-health.png')
+    
 '## Personal History'
 st.write('In late May 2023, my grandfather suffered a hemorrhagic stroke, displaying classic symptoms that, unfortunately, went unrecognized by my family due to our lack of awareness. A month prior, he began experiencing double vision in both eyes, a sign we now know we should have taken more seriously. About a month before he had the stroke, my grandpa started having double vision in both eyes. The doctors noticed blood, but assumed that it would eventually reabsorb back into the brain.')
 st.write('Towards the end of May, he developed a severe migraine out of nowhere. My grandparents decided to give it few days to resolve itself, but the turning point was when my grandpa forgot how to dress himself for work and dropped his sandwich unknowingly while walking into the office. After arriving to the ER, he was eventually given surgery to remove some of the blood on one side of his head. For two weeks in the ICU, he seemed in a vegetative state. Though he briefly woke up, doctors were not optimistic, prompting funeral arrangements and a decision to transfer him to hospice.')
