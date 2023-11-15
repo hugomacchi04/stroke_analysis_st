5b040b8f48fdf48bd40447fb2de58d7b428acd66
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 15:29:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
@@ -37,13 +36,13 @@ y = df['stroke']  # stroke
 # split the dataset into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# Build a Logistic Regression Classifier
+# build a Logistic Regression Classifier
 logistic_model = LogisticRegression(random_state=42)
 
-# Model training
+# train the model
 logistic_model.fit(x_train, y_train)
 
-# Predict Output
+# write accuracy
 y_pred = logistic_model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 f'##### Current Model Accuracy: {round(accuracy, 2)}'
