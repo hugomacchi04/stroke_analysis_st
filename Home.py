e619645f47550dd28671d4e1d66ef3592a3767dd
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2023-11-15 16:02:14 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 '## What is a stroke?'
+st.write('A stroke may happen when there is a blockage in the blood flow to the brain or if there is sudden bleeding within the brain. There are four types of strokes: ischemic strokes, hemorrhagic strokes, transient ischemic attacks, cryptogenic strokes and brain stem strokes.')
 
 '## What are symptoms of a stroke?'
 
@@ -10,4 +11,6 @@ import streamlit as st
 
 '### Additional Resources'
 
+'##### References'
+st.write('https://www.nhlbi.nih.gov/health/stroke')
 
